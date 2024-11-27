f758633b568257fca30ba5ef7fedb0e315147be5
MindustryLogic <xyndermultilanguagelism@gmail.com>
2024-11-27 14:01:53 +0900

    ok it loops

--- print.py
+++ print.py
@@ -7,8 +7,20 @@ print("The actual programm thing start here and i will not remove this part")
 #wait how do i actually tell the code to seperate a equation
 #ya basic math operator should seperate em
 #before that do the basic of basic
-a = float(input("number here please"))
-b = float(input("number here please"))
-print("this is addition ")
-result = a + b
-print(f"{a}+{b}={result}")
+def b_add():
+ a = float(input("number here please"))
+ b = float(input("number here please"))
+ print("this is addition ")
+ result = a + b
+ print(f"{a}+{b}={result}")
+ inf()
+ 
+def inf():
+ print("continue? Y/n")
+ more = input("Y/n")
+ if "Y" in more:
+    b_add()
+ if "n" in more:
+    exit
+ 
+b_add()
