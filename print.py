5324901f1b8ef37a0d6d75bd93f2400e26c1cdf9
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2024-11-27 10:57:39 +0800

    problem mathway?

--- print.py
+++ print.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 print("hello world")
+import time
+time.sleep(5)
+print("The actual programm thing start here and i will not remove this part")
+#wait how do i actually tell the code to seperate a equation
+#ya basic math operator should seperate em
+#before that do the basic of basic
+a = float(input("number here please"))
+b = float(input("number here please"))
+print("this is addition ")
+result = a + b
+print(f"{a}+{b}={result}")
