b3b4d91a1aa0e874a5b56f602c5986a63b6900d9
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2024-11-28 00:40:28 +0000

    nope chrome lags

--- stuff/print.py
+++ stuff/print.py
@@ -31,28 +31,37 @@ def inf():
  
  
 def mode_sele():
- print("work for currently working stuff, exp for indev mode ig")
+ print("work for currently working stuff, exp for indev mode ig, and exit kill the programm")
  mode = input("uh what mode u want use type it exactly idk ")
  if "work" in mode:
    b_add()
+ if "exit" in mode:
+    exit
  else :
     temp()
     
     
 def temp():
     eq_input = input("type equation here: ")
+    eq_input_first_fix = ""
     op_included_start = eq_input.find("+" or "-", 0, 1)
     eq_input_1st_num = eq_input[0]
     if "-1" == op_included_start:
-       eq_input.replace(eq_input[0], +eq_input[0])
+       eq_input_first_fix = "9"
+    else :
+       eq_input_first_fix = eq_input
+
     print(op_included_start)
     print(eq_input_1st_num)
+    print(eq_input[0])
+    print(eq_input_first_fix)
     print(list(op_id(eq_input, "+")))
     print(list(op_id(eq_input, "-")))
     print(list(op_id(eq_input, "*")))
     print(list(op_id(eq_input, "/")))
     num_count = len(list(op_id(eq_input, "+"))) + len(list(op_id(eq_input, "-"))) + len(list(op_id(eq_input, "*"))) + len(list(op_id(eq_input, "/")))
     print(num_count)
+    mode_sele()
 
     
 
